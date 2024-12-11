427cb1115ec5d9e66def0fba10351548bf41324d
Yevhen Shashnin <genyadstph@gmail.com>
2024-12-11 17:51:03 +0000

    redeploy

--- src/modules/fanat/Dashboard/Dashboard.tsx
+++ src/modules/fanat/Dashboard/Dashboard.tsx
@@ -8,7 +8,7 @@ import {CreatorStats} from "@/modules/fanat/Dashboard/sections/creatorTable";
 
 const Dashboard = () => {
     return (
-        <div className="max-w-[1600px] mx-auto grid grid-cols-1 md:grid-cols-4 gap-2 ">
+        <div className="w-full max-w-[1600px] mx-auto grid grid-cols-1 md:grid-cols-4 gap-2 ">
             <Settings/>
             <Earnings/>
             <EarningTrends/>
